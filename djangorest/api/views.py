34f2ae7fc59f142d6c28c98a09be738db7362c73
dafma <mar_tp@hotmail.es>
2020-09-29 13:40:21 -0500

    try put, post, delete to models

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
@@ -12,14 +13,33 @@ class MovieViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
-        if 'hola' in request.data:
-            movie = Movie.objects.get(id=pk)
-            print('movie is', movie.title)
-            response = {'message':'its workingg'}
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        if 'stars' in request.data:
+
+            movie = Movie.objects.get(id=2)
+            # lee datos del request
+            stars = request.data['stars']
+            # user = request.user
+            user = User.objects.get(pk=1)
+            print(user)
+
+            # actualiza si no crea
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.starts = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message':'Rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+       
+            except:
+                Rating.objects.create(user=user, movie=movie, starts=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+               
         else:
             response = {'message':'you need to provide hola'}
-            return Response(response, status=status.HTTP_200_OK)
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
         
 class RatingViewSet(viewsets.ModelViewSet):
